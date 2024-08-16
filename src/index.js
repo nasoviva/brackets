81d0d4e58a18b94844c379b87a24516ce48537aa
nasoviva <nasoviva@gmail.com>
2024-08-16 13:49:48 -0300

    feat: func is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let currentStr = str;
+  let prevStr;
+
+  while (prevStr !== currentStr) {
+    prevStr = currentStr;
+    for (const [open, close] of bracketsConfig) {
+      const pair = open + close;
+      while (currentStr.includes(pair)) {
+        currentStr = currentStr.replace(pair, '');
+      }
+    }
+  }
+
+  return currentStr.length === 0;
 }
